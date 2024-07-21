f863c138660d2c36c02526abf525a6f194ec8e62
TakeshiKanazawa <oseans@hotmail.co.jp>
2024-07-21 22:16:53 +0900

    ハンバーガーメニュー実装（仮）

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+"use strict";
+$(function () {
+  const hamburger = $(".hamburger");
+  const nav = $(".nav");
+
+  hamburger.click(function () {
+    $(this).find(".hamburger-bar").toggleClass("is_active");
+    nav.toggleClass("is_active");
+  })
+})
