d86717a99645777c58c8cb01feb60b6508ef4fe4
TakeshiKanazawa <oseans@hotmail.co.jp>
2024-08-14 13:42:00 +0900

    BBCが選ばれる理由セクションの実装

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 
 "use strict";
 $(function () {
+  // ハンバーガーmenu
   const hamburger = $(".hamburger");
   const nav = $(".nav");
 
@@ -9,4 +10,24 @@ $(function () {
     nav.toggleClass("is_active");
 
   })
+  // InView
+  // BBCが選ばれる理由(スライド左)
+  // 要素が表示領域に入った時にクラスを要素に追加する
+  // inviewイベントが発生したときに関数を実行
+  // inviewイベントは、要素がビュー(表示領域)に入った時にトリガーされる
+  $('.inview-slide-left').on('inview', function (event, isInView, visiblePartX, visiblePartY) {
+    if (isInView)
+      // 要素が表示されたらslide-leftクラスを追加
+      // stop()はアニメーションの競合や意図しない動作を防ぐ為
+      $(this).stop().addClass('slide-left');
+  });
+
+  // スライド右
+    $('.inview-slide-right').on('inview', function (event, isInView, visiblePartX, visiblePartY) {
+    if (isInView)
+      // 要素が表示されたらslide-leftクラスを追加
+      // stop()はアニメーションの競合や意図しない動作を防ぐ為
+      $(this).stop().addClass('slide-right');
+  });
+  
 })
