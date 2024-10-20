eae48e62a28535037633be0ba71f91b2375ebc48
TakeshiKanazawa <oseans@hotmail.co.jp>
2024-10-20 09:06:34 +0900

    ソース修正

--- js/main.js
+++ js/main.js
@@ -1,39 +1,59 @@
-
-"use strict";
-$(function () {
-  // ハンバーガーmenu
-  const hamburger = $(".hamburger");
-  const nav = $(".nav");
-
-  hamburger.click(function () {
-    $(this).find(".hamburger-bar").toggleClass("is_active");
-    nav.toggleClass("is_active");
-
+$(function(){
+  /*=================================================
+  ハンバーガ―メニュー
+  ===================================================*/
+  // ハンバーガーメニューをクリックした時
+  $('.hamburger').on('click', function() {
+    // ハンバーガーメニューの共通処理を呼び出す
+    hamburger();
   });
-  // InView
-  // BBCが選ばれる理由(スライド左)
-  // 要素が表示領域に入った時にクラスを要素に追加する
-  // inviewイベントが発生したときに関数を実行
-  // inviewイベントは、要素がビュー(表示領域)に入った時にトリガーされる
-  $('.inview-slide-left').on('inview', function (event, isInView, visiblePartX, visiblePartY) {
-    if (isInView)
+  // メニューのリンクをクリックした時
+  $('#navi a').on('click', function() {
+    // ハンバーガーメニューの共通処理を呼び出す
+    hamburger();
+  });
+
+  /*=================================================
+  Inview（画面に表示されたタイミングで処理を実行）
+  ===================================================*/
+  // BBBが選ばれる理由（スライド左）
+  $('.inview-slide-left').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
+    if(isInView){
       // 要素が表示されたらslide-leftクラスを追加
-      // stop()はアニメーションの競合や意図しない動作を防ぐ為
       $(this).stop().addClass('slide-left');
+    }
   });
-
-  // スライド右
-    $('.inview-slide-right').on('inview', function (event, isInView, visiblePartX, visiblePartY) {
-    if (isInView)
-      // 要素が表示されたらslide-leftクラスを追加
-      // stop()はアニメーションの競合や意図しない動作を防ぐ為
+  // BBBが選ばれる理由（スライド右）
+  $('.inview-slide-right').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
+    if(isInView){
+      // 要素が表示されたらslide-rightクラスを追加
       $(this).stop().addClass('slide-right');
-    });
-  // 受講生の声(吹き出し)
-      $('.inview-balloon').on('inview', function (event, isInView, visiblePartX, visiblePartY) {
-    if (isInView)
-      // 要素が表示されたらslide-leftクラスを追加
-      // stop()はアニメーションの競合や意図しない動作を防ぐ為
+    }
+  });
+  // 受講生の声（ふきだし）
+  $('.inview-balloon').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
+    if(isInView){
+      // 要素が表示されたらballoonクラスを追加
       $(this).stop().addClass('balloon');
-    });
+    }
+  });
 });
+
+/*=================================================
+ハンバーガ―メニュー共通処理
+===================================================*/
+// ハンバーガーメニューをクリックした時とメニュー内のリンクをクリックした時の
+// 処理が同じなので処理を共通化する
+function hamburger() {
+  // toggleClassを使用することで、hamburgerクラスにactiveクラスが存在する場合は削除、
+  // 存在しない場合を追加する処理を自動で行ってくれる
+  $('.hamburger').toggleClass('active');
+
+  if ($('.hamburger').hasClass('active')) {
+    // hamburgerクラスにactiveクラスが存在する場合は、naviにもactiveクラスを追加する
+    $('#navi').addClass('active');
+  } else {
+    // hamburgerクラスにactiveクラスが存在しない場合は、naviからactiveクラスを削除する
+    $('#navi').removeClass('active');
+  }
+}
